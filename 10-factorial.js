5069bb87ccfbb4cfbfeaeccf36381ebed5643e3e
anas amellahe <amellaheanas@gmail.com>
2025-05-23 19:39:04 +0100

    create a factorial function

--- 10-factorial.js
+++ 10-factorial.js
@@ -1,13 +1,18 @@
 const { argv } = require("process")
-
 let num =  +argv[2]
 
-if (isNaN(num))
-        console.log(1)
-else
+
+function factorial(num)
 {
-    let res = 1;
-    for (;num > 0; num--)
-        res *= num
-    console.log(res) 
+    if (isNaN(num))
+            console.log(1)
+    else
+    {
+        let res = 1;
+        for (;num > 0; num--)
+            res *= num
+        console.log(res) 
+    }
 }
+
+factorial(num)
